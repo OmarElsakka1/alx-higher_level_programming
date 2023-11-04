be21e7a2cf6af24fd876c63e1cfed687b94d42d2
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-04 00:18:29 -0700

    No c or C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    out = ""
+    output = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            out += char
-    return out
+            output += char
+    return output
