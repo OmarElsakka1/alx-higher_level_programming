ea954b8d36a94ee0766cada2ac1e63818a28c14e
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-21 00:31:29 +0000

    raise exception

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 raise_exception = __import__('5-raise_exception').raise_exception
-
 try:
     raise_exception()
-except TypeError as te:
+except TypeError:
     print("Exception raised")
