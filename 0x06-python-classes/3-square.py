82ff9d74b413b9526f2bd307a67c8b2da5e46119
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-23 00:53:54 +0000

    Square Area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """This defines a square class"""
+
+    def area(self):
+        """Returning the area of the square instance"""
+        return self.__size * self.__size
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
