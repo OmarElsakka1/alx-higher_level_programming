a70ed7562c1f0632ebf6bc15bbfa94e5198580b9
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-23 00:47:32 +0000

    Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """This Defines the square class"""
+
+    def __init__(self, size):
+        """
+        Args:
+            size: size of square
+        """
+        self.__size = size
