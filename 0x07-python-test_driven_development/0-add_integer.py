53a72aef216b43ed6a569282d01c87c4b8bf4951
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-23 13:16:11 +0000

    First TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add function module"""
+
+def add_integer(a, b=98):
+    """Adds 2 int or float values, return int"""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
