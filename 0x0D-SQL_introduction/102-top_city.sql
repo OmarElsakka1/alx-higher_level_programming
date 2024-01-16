f1397c8e0eaef04058a48b72ffce58684999d201
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-01-16 04:54:32 -0800

    Hottest 3 cities

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Printing the highest 3 cities in temperatures between July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
